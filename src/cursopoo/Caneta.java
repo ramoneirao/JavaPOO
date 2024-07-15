f09b086e5b628a00665ac76a4b4da750d4453acb
Ramon Mendes <ramon.mendes@icen.ufpa.br>
2024-07-15 20:39:05 -0300

    Getter, Setter e Construtor
    Construindo os métodos Getter, Setter e Construtor

--- src/cursopoo/Caneta.java
+++ src/cursopoo/Caneta.java
@@ -2,17 +2,52 @@ package cursopoo;
 
 public class Caneta {
     
-    public String modelo;
-    public String cor;
+    private String modelo;
+    private String cor;
     private float ponta;
     protected int carga;
     private boolean tampada;
+
+    public Caneta(String modelo, String cor, float ponta) {
+        this.tampar();
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ponta = ponta;
+    }
+    
+    public String getModelo(){
+        return this.modelo;
+    }
+    public void setModelo (String m){
+        this.modelo = m;
+    }
+      
+    public String getCor(){
+        return this.cor;
+    }
+    public void setCor(String c){
+        this.cor = c;
+    }
+    
+    public float getPonta(){
+        return this.ponta;
+    }
+    public void setPonta(float p){
+        this.ponta = p;
+    }
+    
+    public int getCarga(){
+         return this.carga;
+    }
+    public void setCarga(int c){
+        this.carga = c;
+    }
     
     public void status(){
-        System.out.println("Modelo " + this.modelo);
-        System.out.println("Uma caneta " + this.cor);
-        System.out.println("Ponta: " + this.ponta);
-        System.out.println("Carga: " + this.carga);
+        System.out.println("Modelo " + this.getModelo());
+        System.out.println("Uma caneta " + this.getCor());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Carga: " + this.getCarga());
         System.out.println("Esta tampada? " + this.tampada);
         
     }
--- src/cursopoo/Main.java
+++ src/cursopoo/Main.java
@@ -2,13 +2,7 @@ package cursopoo;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Ola, mundo!");
-        Caneta c1 = new Caneta();
-        c1.modelo = "BIC";
-        c1.cor = "Azul";
-        //c1.ponta = 0.5f; Ponta é privado
-        c1.carga = 80;
-        //c1.tampada = true;
+        Caneta c1 = new Caneta("Bic","Azul",0.5f);
         c1.status();
     }
 }
